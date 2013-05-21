2b37d1fc1dc97b8fb8ebd52dac819569446bd383
Dwight Dickinson <dwightpro@gmail.com>
2013-05-21 11:19:00 -0400

    Initial Commit
    Removed randomization
    Added new methods

---
+++ dragon.py
@@ -0,0 +1,63 @@
+import random
+import time
+
+def displayIntro():
+	print ('You are on a planet full of dragons. In front of you,')
+	print ('you see two caves. In one cave, the dragon is friendly')
+	print ('and will share his treasure with you. The other dragon')
+	print ('is greedy and hungry, and will eat you on sight.')
+	print
+	
+	
+def chooseCave():
+	cave = ''
+	while cave != '1' and cave != '2' and cave != '3':
+		print ('Which cave will you go into? (1 or 2)')
+		cave = raw_input()
+	return cave
+
+def checkCave(chosenCave):
+	print ('You approach the cave...')
+	time.sleep(2)
+	print ('It is dark and spooky...')
+	time.sleep(2)
+	print ('A large dragon jumps out in front of you! He opens his jaws and...')
+	print
+	time.sleep(2)
+	
+	if (chosenCave == '1'):
+		cave1()
+	elif (chosenCave == '2'):
+		cave2()
+	elif (chosenCave == '3'):
+		cave3()
+	
+	#if chosenCave == str(friendlyCave):
+	#	print ('Gives you his treasure!')
+	#else:
+	#	print ('Gobbles you down in one bite!')
+
+def cave1():
+	print('Gives you his treasure!')
+	
+def cave2():
+	print('Gobbles you down in one bite!')
+
+def cave3():
+	print('Chases you down and burns you with his breath')
+
+
+def main():
+	
+	
+	playAgain = 'yes'
+	while playAgain == 'yes' or playAgain == 'y':
+		displayIntro()
+		caveNumber = chooseCave()
+		checkCave(caveNumber)
+	
+		print ('Do you want to play again? (yes or no)')
+		playAgain = raw_input()
+
+
+if __name__ == "__main__": main()
