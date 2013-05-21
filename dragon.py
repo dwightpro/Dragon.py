45d09de7780474b3aa69ed2d5ea320b2a221b26b
Dwight Dickinson <dwightpro@gmail.com>
2013-05-21 14:42:37 -0400

    Added new decision levels
    Added new decision levels

--- dragon.py
+++ dragon.py
@@ -1,13 +1,17 @@
-' File: dragon.py
-' Author Name: Dwight Dickinson
-' Last Modified By: Dwight Dickinson
-' Date Last Modified: May 19th 2013
-' Revision History
-' 1.0 - May 19th 2013
-' 	- Added comments and header information
-' 	- Removed the randomization
-'	- Changed the print statements for the first decision
-'	- Changed the method names
+# File: dragon.py
+# Author Name: Dwight Dickinson
+# Last Modified By: Dwight Dickinson
+# Date Last Modified: May 19th 2013
+# Revision History
+# 1.0 - May 19th 2013
+# 	- Added comments and header information
+# 	- Removed the randomization
+#	- Changed the print statements for the first decision
+#	- Changed the method names
+
+# 1.1 - May 20th 2013
+#	- Added second decision level
+#	- Added new functions for final decision level
 
 import time
 
@@ -45,22 +49,55 @@ def firstDecision(chosenCave):
 		print('Which tunnel will you choose? (Left, or Right)')
 		decision = raw_input()
 		cave1(decision)
-	elif chosenCave == '2'
+	elif chosenCave == '2':
 		print('The cave has two holes in the ground...')
 		time.sleep(2)
 		print('The left hole has a ladder for you to climb down, the right appears to be a slide')
 		time.sleep(2)
 		print
-		print('Which hole will you proceed down?')
+		print('Which hole will you proceed down? (Left, or right)')
 		decision = raw_input()
 		cave2(decision)
 
-' These are the decision trees for the second decision
+# These are the decision trees for the second decision
 def cave1(chosenTunnel):
-	print('The ')
+	decision = ''
+	if chosenTunnel.upper() == 'LEFT':
+		print
+		print('You proceed down the left tunnel with caution...')
+		time.sleep(2)
+		print('The light becomes brighter...')
+		time.sleep(2)
+		print('A person carrying a torch appears from the corner with an evil looking smile on his face.')
+		time.sleep(2)
+		print
+		print('Do you run away, or stay put? (run, or stay)')
+		decision = raw_input()
+		light(decision)
+		
+	elif chosenTunnel.upper() == 'RIGHT':
+		print
+		print('You proceed towards the voice...')
+		time.sleep(2)
+		print('The voice begins to get raspy...')
+		time.sleep(2)
+		print('The voice becomes a fierce roar as a dragon appears infront of you...')
+		time.sleep(2)
+		print('Do you run away, or try to fight the dragon. (run, or fight)')
+		print
+		decision = raw_input()
+		fight(decision)
 	
 def cave2(chosenPath):
-	print('The ')
+	print
+	
+	
+#Light decisions
+def light(runOrStay):
+	if runOrStay.upper() == 'RUN':
+		print('You run
+	
+def fight(runOrFight):
 	
 	
 def main():
